e967491d3afbdc9aea253455e357276ae280bca5
MenaceHecker <thedynamic2508@gmail.com>
2026-02-13 15:12:30 -0500

    Config mismatch error encountered

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,17 +7,17 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3         = "http://localhost:4566"
-    sqs        = "http://localhost:4566"
-    sns        = "http://localhost:4566"
-    dynamodb   = "http://localhost:4566"
-    iam        = "http://localhost:4566"
-    lambda     = "http://localhost:4566"
-    sts        = "http://localhost:4566"
-    cloudwatch = "http://localhost:4566"
-    logs       = "http://localhost:4566"
-    ssm        = "http://localhost:4566"
-  }
+  s3         = "http://127.0.0.1:4566"
+  sqs        = "http://127.0.0.1:4566"
+  sns        = "http://127.0.0.1:4566"
+  dynamodb   = "http://127.0.0.1:4566"
+  iam        = "http://127.0.0.1:4566"
+  lambda     = "http://127.0.0.1:4566"
+  sts        = "http://127.0.0.1:4566"
+  cloudwatch = "http://127.0.0.1:4566"
+  logs       = "http://127.0.0.1:4566"
+  ssm        = "http://127.0.0.1:4566"
+}
 }
 
 provider "random" {}
