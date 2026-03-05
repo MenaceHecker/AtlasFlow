58f73a8438f8bc034f5d013dc1a3fb5c0a5e1f4e
MenaceHecker <thedynamic2508@gmail.com>
2026-03-04 21:04:56 -0500

    chore: terraform fmt

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,17 +7,17 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-  s3         = "http://127.0.0.1:4566"
-  sqs        = "http://127.0.0.1:4566"
-  sns        = "http://127.0.0.1:4566"
-  dynamodb   = "http://127.0.0.1:4566"
-  iam        = "http://127.0.0.1:4566"
-  lambda     = "http://127.0.0.1:4566"
-  sts        = "http://127.0.0.1:4566"
-  cloudwatch = "http://127.0.0.1:4566"
-  logs       = "http://127.0.0.1:4566"
-  ssm        = "http://127.0.0.1:4566"
-}
+    s3         = "http://127.0.0.1:4566"
+    sqs        = "http://127.0.0.1:4566"
+    sns        = "http://127.0.0.1:4566"
+    dynamodb   = "http://127.0.0.1:4566"
+    iam        = "http://127.0.0.1:4566"
+    lambda     = "http://127.0.0.1:4566"
+    sts        = "http://127.0.0.1:4566"
+    cloudwatch = "http://127.0.0.1:4566"
+    logs       = "http://127.0.0.1:4566"
+    ssm        = "http://127.0.0.1:4566"
+  }
 }
 
 provider "random" {}
